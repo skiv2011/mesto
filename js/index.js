552641067865c446dec8f584a91b1ca5a77676eb
Pavel Zhukovskii <FiremanDzen@yandex.ru>
2022-05-20 23:40:42 +0300

    Исправить логику и опечатки

--- js/index.js
+++ js/index.js
@@ -4,14 +4,16 @@ const closeButton = document.querySelector('.popup__close-button');
 const nameAvtor = document.querySelector('.profile__title');
 const postAvtor = document.querySelector('.profile__subtitle');
 let formElement = document.querySelector('.popup__form');
-let nameInput = document.querySelector('.popup__destription_type_name');
-let jobInput = document.querySelector('.popup__destription_type_job');
+let nameInput = document.querySelector('.popup__decription_type_name');
+let jobInput = document.querySelector('.popup__decription_type_job');
 
 /* Функция открытия попапа */
 function togglePopup() {
     popup.classList.toggle('popup_opened');
-    nameInput.value = nameAvtor.textContent;
-    jobInput.value = postAvtor.textContent;
+    if (popup.classList.contains('popup_opened')) {
+        nameInput.value = nameAvtor.textContent;
+        jobInput.value = postAvtor.textContent;
+    }
 };
 
 /* Функция закрывающая попап */
